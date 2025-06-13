1b2a863e3ce08c5eb331b9e23152d606e56ab626
Jose Miguel <miguel.it.developer@gmail.com>
2025-06-12 18:55:18 -0600

    Auto-generated commit

---
+++ chunk-BS7POX6Y.js
@@ -0,0 +1,17 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-YEY2H36D.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=chunk-BS7POX6Y.js.map
